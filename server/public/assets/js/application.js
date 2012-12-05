f5439a2107020353d891c7fa2a96b534a9a44c4f
Hicham Rafi <rafitool@hotmail.com>
2012-12-04 18:25:11 -0800

    change selected marker icon

--- server/public/assets/js/application.js
+++ server/public/assets/js/application.js
@@ -16,6 +16,19 @@ L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/
 
 
 
+var SelectedIcon = new L.Icon.Default({
+	iconUrl: './assets/img/marker-icon-selected.png'
+});
+
+var defaultIcon = new L.Icon.Default();
+
+
+var currentMarker;
+var currentEditMarker;
+
+
+
+
 var popup = L.popup();
 
 
@@ -25,47 +38,74 @@ $.ajax({
 	url: "/marker",
 }).done(function(markers) {
 	$.each(markers, function(index, markerData) {
-		var marker = L.marker({
+		var marker = new L.marker({
 			lat: markerData.lat,
 			lng: markerData.lng
 		});
 		marker._id = markerData._id;
 		marker.addTo(map);
 		marker.on('click', onMarkerClick);
+
+		//add marker to the list
+		addMarkerListItem(marker, markerData);
 	});
 });
 
-var currentEditMarker;
+
+function addMarkerListItem(marker, markerData) {
+	var created = new Date(parseInt(markerData._id.slice(0, 8), 16) * 1000);
+		var min = created.getMinutes().toString();
+		min = min.length > 1 ? min : '0' + min;
+		var listItem = '<li class="list-item" id="'+ markerData._id +'"><a  class="list-title" ' + ' href="#">' + markerData.username + '</a> ' + created.toDateString() + ' at ' + created.getHours().toString() + ':' + min + '</li>';
+		
+		$('.nav.nav-list').append(listItem);
+
+		$('#' + markerData._id).on('click', function(e) {
+
+			if(currentEditMarker) currentEditMarker.setIcon(defaultIcon);
+
+			currentEditMarker = marker;
+			currentEditMarker.setIcon(SelectedIcon);
+
+			renderMarkerInfo(markerData);
+		});
+
+}
+
 
 function onMarkerClick(e) {
 
-	currentEditMarker = e.target;
+	if(currentEditMarker) currentEditMarker.setIcon(defaultIcon);
 
+	currentEditMarker = e.target;
+	currentEditMarker.setIcon(SelectedIcon);
 
 	$.ajax({
 		type: "GET",
 		url: "/marker/" + currentEditMarker._id,
-	}).done(function(markerData) {
-		$('#edit-marker-layer').text(markerData.layer);
+	}).done(renderMarkerInfo);
+}
 
-		var created = new Date(parseInt(markerData._id.slice(0,8), 16)*1000);
+
+function renderMarkerInfo(markerData) {
+	$('#edit-marker-layer').text(markerData.layer);
+
+		var created = new Date(parseInt(markerData._id.slice(0, 8), 16) * 1000);
 		$('#edit-marker-username').html('<strong>' + markerData.username + '</strong> added this marker on ' + created.toString());
-		
+
 		var like_count = markerData.like_count;
 		$('#like-count').html('<strong>' + markerData.like_count + '</strong>');
 
 		$('#edit-marker-description').text(markerData.description);
 		$('.edit-marker-popover').show();
-	});
-
 }
 
-var currentMarker;
 
 function onMapClick(e) {
 	//popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map);
 	$('.marker-popover').show();
 	currentMarker = L.marker(e.latlng);
+	currentMarker.setIcon(SelectedIcon);
 	map.addLayer(currentMarker);
 }
 
@@ -73,7 +113,12 @@ map.on('click', onMapClick);
 
 
 $(".popover .close").on('click', function() {
-	if(currentMarker) map.removeLayer(currentMarker);
+
+	if(currentEditMarker) currentEditMarker.setIcon(defaultIcon);
+
+	if(currentMarker) {
+		map.removeLayer(currentMarker);
+	}
 	currentMarker = null;
 	$('.popover').hide();
 });
@@ -91,6 +136,7 @@ $(".submit-marker").click(function() {
 			$('.marker-popover').hide();
 			currentMarker._id = data._id;
 			currentMarker.on('click', onMarkerClick);
+			currentMarker.setIcon(defaultIcon);
 			currentMarker = null;
 
 			$('.marker-form')[0].reset();
@@ -115,16 +161,18 @@ $(".cancel-marker").click(function() {
 
 
 $(".support-marker").click(function() {
-	if(!currentEditMarker)
-		return;
+	if(!currentEditMarker) return;
 
 	$.ajax({
 		type: "PUT",
-		url: "/marker/support/"+currentEditMarker._id,
+		url: "/marker/support/" + currentEditMarker._id,
 		data: {},
 		success: function(markerData) {
 			$('#like-count').html('<strong>' + markerData.like_count + '</strong>');
-			currentMarker = null;
+			
+			//hack for now to avoid multiple support...
+			currentEditMarker.setIcon(defaultIcon);
+			currentEditMarker = null;
 
 		}
 	});
@@ -132,38 +180,3 @@ $(".support-marker").click(function() {
 	return false;
 });
 
-$(function() {
-  // Handler for .ready() called.
-  //load markers to map
-  $.ajax({
-    type: "GET",
-    url: "/marker",
-  }).done(function(markers) {
-    $.each(markers, function(index, markerData) {
-      var marker = L.marker({
-        lat: markerData.lat,
-          lng: markerData.lng
-      });
-      var created = new Date(parseInt(markerData._id.slice(0,8), 16)*1000);
-      var min = created.getMinutes().toString();
-      min = min.length > 1 ? min : '0' + min;
-      var listItem = '<li><a class="list_item" data-marker='+markerData._id+' href="#">'+markerData.username+'</a> ' + created.toDateString() + ' at '+ created.getHours().toString() + ':' + min + '</li>';
-      $('.nav.nav-list').append(listItem).on('click', function(e) {
-        var id = $(e.target).data('marker');
-        $.ajax({
-          type: "GET",
-          url: "/marker/" + id,
-        }).done(function(markerData) {
-          $('#edit-marker-layer').text(markerData.layer);
-          $('#edit-marker-username').html('<strong>' + markerData.username + '</strong> added this marker on ' + created.toString());
-
-          var like_count = markerData.like_count;
-          $('#like-count').html('<strong>' + markerData.like_count + '</strong>');
-
-          $('#edit-marker-description').text(markerData.description);
-          $('.edit-marker-popover').show();
-        });
-      });
-    });
-  });
-});
